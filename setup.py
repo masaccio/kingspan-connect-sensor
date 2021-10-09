d5876103044f4814c88ffb933cde7b19a3b54a71
Jon Connell <python@figsandfudge.com>
2021-10-09 10:57:02 +0100

    Build now setup.cfg based

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
-    name="kingspan-connect-sensor",
-    version="1.0",
-    author="Jon Connell",
-    author_email="python@figsandfudge.com",
-    description="API to get oil tank from Kingspan SENSiT sensors",
-    license='MIT',
-    long_description=open('README.md').read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/masaccio/kingspan-connect-sensor",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
+setup()
