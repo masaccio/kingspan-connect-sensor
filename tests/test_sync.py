46c6dd7221248a4b9f7ea8a555f413e5a00b0ade
Jon Connell <python@figsandfudge.com>
2024-10-26 18:23:56 +0100

    Move all reference files to JSON

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -1,35 +1,21 @@
 from datetime import datetime
-from unittest.mock import patch
-from zeep.transports import Transport
-
-from mock_data import VALID_STATUS, USERNAME, PASSWORD
-from mock_requests import MockResponse, mock_load_remote_data, mock_post_xml
-
-from connectsensor import __version__, SensorClient
 
 import pandas as pd
 
+from mock_data import USERNAME, PASSWORD
+from connectsensor import SensorClient
 
-def test_status():
-    with patch.object(
-        Transport,
-        "_load_remote_data",
-        side_effect=mock_load_remote_data,
-    ) as mock_get, patch.object(
-        Transport,
-        "post_xml",
-        side_effect=mock_post_xml,
-    ) as mock_post_func:
-        client = SensorClient()
-        client.login(USERNAME, PASSWORD)
-        tanks = client.tanks
-        tank_level = tanks[0].level
-        assert tanks[0].level == 1000
-        assert tanks[0].serial_number == "20001000"
-        assert tanks[0].model == "TestModel"
-        assert tanks[0].name == "TestTank"
-        assert tanks[0].capacity == 2000
-        tank_history = pd.DataFrame(tanks[0].history)
-        assert tank_history.reading_date[0] == datetime(2021, 1, 25, 13, 59, 14)
-        assert tank_history.level_percent[1] == 95
-        assert tank_history.level_litres[2] == 1880
+
+def test_status(mock_zeep):
+    client = SensorClient()
+    client.login(USERNAME, PASSWORD)
+    tanks = client.tanks
+    assert tanks[0].level == 1000
+    assert tanks[0].serial_number == "20001000"
+    assert tanks[0].model == "TestModel"
+    assert tanks[0].name == "TestTank"
+    assert tanks[0].capacity == 2000
+    tank_history = pd.DataFrame(tanks[0].history)
+    assert tank_history.reading_date[0] == datetime(2021, 1, 25, 13, 59, 14)
+    assert tank_history.level_percent[1] == 95
+    assert tank_history.level_litres[2] == 1880
