76249cdaa39362960719a9decd486cb6984104c4
Jon Connell <python@figsandfudge.com>
2026-02-25 14:09:17 +0000

    New exceptions

--- src/connectsensor/__init__.py
+++ src/connectsensor/__init__.py
@@ -1,6 +1,10 @@
 import importlib.metadata
 
 from connectsensor.client import AsyncSensorClient, SensorClient  # noqa: F401
-from connectsensor.exceptions import APIError, DBError  # noqa: F401
+from connectsensor.exceptions import (
+    KingspanAPIError,
+    KingspanInvalidCredentials,
+    KingspanDBError,
+)
 
 __version__ = importlib.metadata.version("kingspan-connect-sensor")
