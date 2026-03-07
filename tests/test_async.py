3f3102defb75dc56edb5da38603c6ef55b96b6c4
Jon Connell <python@figsandfudge.com>
2026-03-07 11:34:54 +0000

    Test both APIs

--- tests/test_async.py
+++ tests/test_async.py
@@ -6,6 +6,7 @@ import pytest
 
 from conftest import get_mock_filename, get_mock_response
 from connectsensor import (
+    APIVersion,
     AsyncSensorClient,
     KingspanAPIError,
     KingspanInvalidCredentialsError,
@@ -15,19 +16,23 @@ from mock_data import PASSWORD, USERNAME
 
 @pytest.mark.asyncio
 async def test_status(mock_async_httpx_post):
-    async with AsyncSensorClient() as client:
-        await client.login(USERNAME, PASSWORD)
-        tanks = await client.tanks
-        assert await tanks[0].level == 1000
-        assert await tanks[0].serial_number == "20001000"
-        assert await tanks[0].model == "TestModel"
-        assert await tanks[0].name == "TestTank"
-        assert await tanks[0].capacity == 2000
+    async def check_client(api_version: APIVersion) -> None:
+        async with AsyncSensorClient(api_version) as client:
+            await client.login(USERNAME, PASSWORD)
+            tanks = await client.tanks
+            assert await tanks[0].level == 1000
+            assert await tanks[0].serial_number == "20001000"
+            assert await tanks[0].model == "TestModel"
+            assert await tanks[0].name == "TestTank"
+            assert await tanks[0].capacity == 2000
 
-        tank_history = await tanks[0].history
-        assert tank_history[0]["reading_date"] == datetime(2021, 1, 25, 13, 59, 14)
-        assert tank_history[1]["level_percent"] == 95
-        assert tank_history[2]["level_litres"] == 1880
+            tank_history = await tanks[0].history
+            assert tank_history[0]["reading_date"] == datetime(2021, 1, 25, 13, 59, 14)
+            assert tank_history[1]["level_percent"] == 95
+            assert tank_history[2]["level_litres"] == 1880
+
+    await check_client(APIVersion.CONNECT_V1)
+    await check_client(APIVersion.KNECT_V1)
 
 
 @pytest.mark.asyncio
@@ -82,6 +87,7 @@ async def test_history_exception(mocker):
             _ = await tanks[0].history
 
 
+@pytest.mark.asyncio
 async def test_debug_redaction(mock_sync_httpx_post, caplog):  # noqa: ARG001
     caplog.set_level(logging.DEBUG, logger="connectsensor")
     async with AsyncSensorClient() as client:
