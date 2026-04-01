a17f004b70434478c67b4b392b7fd49d991b824d
Jon Connell <python@figsandfudge.com>
2026-04-01 17:51:23 +0100

    Remove stubs in favour of hints

--- src/connectsensor/__init__.pyi
+++
@@ -1,6 +0,0 @@
-from connectsensor.exceptions import *
-from _typeshed import Incomplete
-from connectsensor.client import AsyncSensorClient as AsyncSensorClient, SensorClient as SensorClient
-from connectsensor.const import APIVersion as APIVersion
-
-__version__: Incomplete
