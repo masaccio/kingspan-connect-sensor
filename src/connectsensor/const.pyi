073b9376d54e8bddb5c04a490ed85ef1d72d9f98
Jon Connell <python@figsandfudge.com>
2026-04-01 17:47:45 +0100

    Updated stubs

--- src/connectsensor/const.pyi
+++ src/connectsensor/const.pyi
@@ -1,16 +1,22 @@
-from datetime import datetime
-from typing import TypeAlias
-
 from _typeshed import Incomplete
+from enum import IntEnum
+from typing import TypeAlias
 
 API_SERVER: str
 API_PORT: int
 API_BASE_URL: Incomplete
+DEFAULT_SERVER: str
+WSDL_PATH: str
+WSDL_URL: Incomplete
+SOAP_NS_PREFIX: str
+
+class APIVersion(IntEnum):
+    CONNECT_V1 = ...
+    KNECT_V1 = ...
+
+DEFAULT_API_VERSION: Incomplete
 HTTP_UNAUTHORIZED: int
 TOKEN: str
-
-APIResponseValue: TypeAlias = (
-    str | int | datetime | list[APIResponseValue] | dict[str, APIResponseValue]
-)
+APIResponseValue: TypeAlias
 APIRequest: TypeAlias = dict[str, APIResponseValue]
 APIResponse: TypeAlias = dict[str, APIResponseValue]
